135e27e33350a8879777912cfa633ad2041eab83
Anish Gurung <anish.gurung@msu.edu.in>
2023-07-06 11:33:29 +0530

    pushing new files

---
+++ rahul1.py
@@ -0,0 +1,14 @@
+'''
+A simple script
+'''
+def myfunction():
+    '''
+    A simple function
+    '''
+    #printind and declaring number
+    first_num = 4
+    second_num = 3
+    print(first_num)
+    print(second_num)
+ #gave a line
+ myfunction()   
---
+++ text_cap1.py
@@ -0,0 +1,14 @@
+import unittest  
+import cap
+
+class TestCap(unittest.TestCase):
+    def test_one_word(self):
+        text = 'rahul'
+        result = cap.cap_text(text)
+        self.assertEqual(result,'Rahul')
+    def text_multiple_word(self):
+        text = 'rahul gurung'
+        result = cap.cap_text(text)
+        self.assertEqual(result,'Rahul Gurung')
+if __name__ == '__main__'
+    unittest.main()        
